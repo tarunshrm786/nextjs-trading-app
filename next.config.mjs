886b74fb495fe7950b58421b5edfa97f165e4924
Tarun Sharma <tarunshrm768@gmail.com>
2024-10-08 14:07:54 +0530

    commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
 
-export default nextConfig;
+// export default nextConfig;
+
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true, // Enable React Strict Mode for catching potential issues
+    swcMinify: true, // Enable SWC compiler for faster builds
+    // Any additional configurations can be added here
+  };
+  
+  export default nextConfig;
+  
